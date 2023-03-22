14dbe4921f212c93ecd2341be0cc7ad98b517f5c
Ali Poursaeid <alipoursaeid@proton.me>
2023-03-22 11:18:28 +0330

    Add constants for cwd and public folder

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,1 +1,8 @@
+// Sub modules
 export * as env from "./env"
+
+// Constants
+export const CWD = process.cwd() // Current working directory
+export const PUBLIC_FOLDER_PATH = `${CWD}/public`
+export const CITIES_PATH = `${PUBLIC_FOLDER_PATH}/cities.json`
+export const DISTRICTS_PATH = `${PUBLIC_FOLDER_PATH}/districts.json`
