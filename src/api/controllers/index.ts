ded9f885ce189c94c3cef3b5d71ddba90a352b6c
Ali Poursaeid <alipoursaeid@proton.me>
2023-03-28 16:14:08 +0330

    styles: change module name

--- src/api/controllers/index.ts
+++ src/api/controllers/index.ts
@@ -1,6 +1,6 @@
 import { RequestHandler } from "express";
 // Sub modules
-export * as errorController from './error-controller'
+export * as errorController from './error-controllers'
 // '/api' index controller
 export const mainApiController: RequestHandler = (req, res, next) => {
     res.send('Hello There')
