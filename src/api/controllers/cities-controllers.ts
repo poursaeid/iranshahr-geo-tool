b2356931f0c53b7d3bde7afda4ea4fa6ea3b0ef7
Ali Poursaeid <alipoursaeid@proton.me>
2023-03-30 13:27:03 +0330

    feat: get all cities data

--- src/api/controllers/cities-controllers.ts
+++ src/api/controllers/cities-controllers.ts
@@ -1,3 +1,17 @@
 import { RequestHandler } from "express";
+import { jsonReader } from "../utils";
+import { CITIES_PATH } from "../../configs";
+import { getJSONRes } from "../../types";
 
-export const getAll: RequestHandler = (req, res, next) => { }
+export const getAll: RequestHandler = (req, res, next) => {
+    // Get cities data
+    jsonReader(CITIES_PATH, (err, data) => {
+        // Check for errors and pass it to the next error handler if there is any.
+        if (err) next(err)
+        // Send data to the client
+        else {
+            const toSend = getJSONRes(req, { success: true, status: 200, data: data })
+            res.status(toSend.status).send(toSend)
+        }
+    })
+}
