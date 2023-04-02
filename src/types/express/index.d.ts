4e55eb5eab00a8b429a1e3623248f022b42f383f
Ali Poursaeid <alipoursaeid@proton.me>
2023-04-02 15:25:18 +0330

    type based on cities data

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -2,8 +2,18 @@
  * Custom express type to pass local data to the request object
  */
 
+interface ICities {
+    // [key: string]: string | number | Array
+    en: string
+    fa: string
+    ckb: string
+    ar: string
+    latitude: number
+    longitude: number
+    elevation: number
+}
 interface ILocals {
-    cities: object
+    cities: Array<ICities>
 }
 
 declare namespace Express {
