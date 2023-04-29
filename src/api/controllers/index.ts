10545e2de2179ef68ad31c2e539172b46309d6d8
Ali Poursaeid <alipoursaeid@proton.me>
2023-04-30 01:56:04 +0330

    feat: redirect Index page to the /api end point

--- src/api/controllers/index.ts
+++ src/api/controllers/index.ts
@@ -6,8 +6,12 @@ export * as errorController from './error-controllers'
 export * as citiesController from './cities-controllers'
 export * as districtsController from './districts-controller'
 
+// Redirect Index page to the /api end point
+export const indexRedirectController: RequestHandler = (req, res, next) => res.redirect('/api')
+
 // '/api' index controller
 export const mainApiController: RequestHandler = (req, res, next) => {
     const toSend = getJSONRes(req, { status: 200, success: true, messsage: 'Hello There' })
     res.status(toSend.status).send(toSend)
 }
+
--- src/setup-express.ts
+++ src/setup-express.ts
@@ -2,12 +2,13 @@ import express from 'express'
 import cors from 'cors'
 
 import router from './api/routes'
-import { errorController } from './api/controllers'
+import { errorController, indexRedirectController } from './api/controllers'
 // Setup express app
 const app = express()
 // Middleware to Enable All CORS Requests
 app.use(cors())
 // Routes
+app.all('/', indexRedirectController)
 app.use('/api', router)
 // Error handllers
 app.use(errorController.get404)
