7644333548862c85ef84e9c32a499c2048655c52
Ali Poursaeid <alipoursaeid@proton.me>
2023-04-29 21:41:51 +0330

    add type any for jason reader data

--- src/api/utils/index.ts
+++ src/api/utils/index.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 // TODO ADD FN TYPE FOR THE CB
 // https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html#optional-parameters-in-callbacks
-export const jsonReader = (filePath: string, cb: (err: any, data: Array<object>) => void) => {
+export const jsonReader = (filePath: string, cb: (err: any, data: any) => void) => {
     // Read the file asynchronously
     fs.readFile(filePath, 'utf-8', (err, fileData) => {
         if (err) return cb(err, [])
