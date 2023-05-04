899ed83e4ee2629808330a2a93203a639fe3402f
Ali Poursaeid <alipoursaeid@proton.me>
2023-05-04 17:18:02 +0330

    comments format

--- src/api/controllers/index.ts
+++ src/api/controllers/index.ts
@@ -6,12 +6,11 @@ export * as errorController from './error-controllers'
 export * as citiesController from './cities-controllers'
 export * as districtsController from './districts-controller'
 
-// Redirect Index page to the /api end point
+/* Redirect Index page to the /api end point */
 export const indexRedirectController: RequestHandler = (req, res, next) => res.redirect('/api')
 
-// '/api' index controller
+/* '/api' index controller */
 export const mainApiController: RequestHandler = (req, res, next) => {
     const toSend = getJSONRes(req, { status: 200, success: true, messsage: 'Hello There' })
     res.status(toSend.status).send(toSend)
 }
-
