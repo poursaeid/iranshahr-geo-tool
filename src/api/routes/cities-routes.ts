1dd1b6382ad88ec9b5e03cf4d21bb793bb3f0e63
Ali Poursaeid <alipoursaeid@proton.me>
2023-05-04 19:45:44 +0330

    feat: openapi docs for cities api

--- src/api/routes/cities-routes.ts
+++ src/api/routes/cities-routes.ts
@@ -6,9 +6,66 @@ const router = Router()
 // Middlewares
 router.use(passCitiesDataMiddleware)
 
-// Routes : '/api/cities'
+/* Routes : '/api/cities' */
+
+/**
+ * @openapi
+ * '/api/cities':
+ *  get:
+ *      description: Information about all cities and their names in supported languages.
+ *      responses:
+ *          200:
+ *              description: Returns an array of objects.
+ *          500: 
+ *              description: Internal server Error.
+ */
 router.get('/', citiesController.getAll)
+
+/**
+ * @openapi
+ * '/api/cities/list/{lang}':
+ *  get:
+ *      description: List of cities by selected language.
+ *      parameters: 
+ *          - in: path
+ *            name: lang
+ *            required: true
+ *            description: Based on supported languages.
+ *      responses:
+ *          200:
+ *              description: Returns an array of strings.
+ *          422:
+ *              description: Client error for entering wrong parameter.
+ *          500: 
+ *              description: Internal server Error.
+ */
 router.get('/list/:lang', citiesController.getCititesListByLang)
+
+/**
+ * @openapi
+ * '/api/cities/{city}/{lang}':
+ *  get:
+ *      description: Returns information about a specific city based on the language parameter.
+ *      parameters: 
+ *          - in: path
+ *            name: city
+ *            required: true
+ *            description: A string of available city.
+ *          - in: path
+ *            name: langs
+ *            required: true
+ *            description: A string of supported languages.
+ *      responses:
+ *          200:
+ *              description: Returns an object.
+ *          404:
+ *              description: No data.
+ *          422:
+ *              description: Client error for entering wrong parameter.
+ *          500: 
+ *              description: Internal server Error.
+ */
 router.get('/:city/:lang', citiesController.getCityByLang)
+
 // To use the require() method
 export = router
--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -6,7 +6,20 @@ const router = Router()
 /* Base path : /api */
 router.get('/', indexRedirectController)
 router.use('/cities', require('./cities-routes'))
+
+/**
+ * @openapi
+ * '/api/cities/langs':
+ *   get:
+ *     description: Supported Languages
+ *     responses:
+ *       200:
+ *         description: Returns an array of strings.
+ *       500: 
+ *         description: Internal server Error.
+ */
 router.get('/cities/langs', citiesController.getLangs) // get langs without using the passCitiesDataHandler Middleware
+
 router.use('/districts/fa', require('./districts-routes'))
 
 export default router
