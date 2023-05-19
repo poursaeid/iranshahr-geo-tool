e0a3891289f065dbffba8dbea5a812b590fb0955
Ali Poursaeid <alipoursaeid@proton.me>
2023-05-19 17:33:50 +0330

    refactor: read the files synchronously

--- src/api/Middlewares/index.ts
+++ src/api/Middlewares/index.ts
@@ -4,26 +4,20 @@ import { CITIES_PATH, DISTRICTS_PATH } from "../../configs";
 
 /** Read and pass the cities data to the request object */
 export const passCitiesDataMiddleware: RequestHandler = (req, res, next) => {
-    jsonReader(CITIES_PATH, (err, data) => {
-        // Check for errors and pass it to the next error handler if there is any.
-        if (err) next(err)
-        // Pass data to the next request object
-        else {
-            req.locals = { ...req.locals, cities: data }
-            next()
-        }
-    })
+    let { error, data } = jsonReader(CITIES_PATH)
+    // Check for errors and pass it to the next error handler if there is any.
+    if (error) next(error)
+    // Pass data to the next request object
+    req.locals = { ...req.locals, cities: data }
+    next()
 }
 
 /** Read and pass the districts data to the request object */
 export const passDistrictsDataMiddleware: RequestHandler = (req, res, next) => {
-    jsonReader(DISTRICTS_PATH, (err, data) => {
-        // Check for errors and pass it to the next error handler if there is any.
-        if (err) next(err)
-        // Pass data to the next request object
-        else {
-            req.locals = { ...req.locals, districts: data }
-            next()
-        }
-    })
+    let { error, data } = jsonReader(DISTRICTS_PATH)
+    // Check for errors and pass it to the next error handler if there is any.
+    if (error) next(error)
+    // Pass data to the next request object
+    req.locals = { ...req.locals, districts: data }
+    next()
 }
