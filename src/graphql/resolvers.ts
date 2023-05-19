68b450b0b8c71dcbdb6eec79519e707a83e1f0ea
Ali Poursaeid <alipoursaeid@proton.me>
2023-05-19 17:37:55 +0330

    feat: add cities graphql resolver

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,5 +1,12 @@
+import { jsonReader } from "../utils";
+import { CITIES_PATH } from "../configs";
+
 export default {
     Query: {
-        hello: () => 'world',
+        cities: () => {
+            let { error, data } = jsonReader(CITIES_PATH)
+            if (error) console.log(error);
+            else return data
+        },
     },
 };
